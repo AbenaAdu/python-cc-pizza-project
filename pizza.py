c579df2739e2fa9a0027bafbbc3c3a57e418dbba
Abena Adu <abeanadu@gmail.com>
2024-03-09 15:15:47 -0500

    Created new 2D list of toppings and price

--- pizza.py
+++ pizza.py
@@ -1,5 +1,11 @@
 #Toppings for pizza
-toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+toppings = ["pepperoni", 
+            "pineapple", 
+            "cheese", 
+            "sausage", 
+            "olives", 
+            "anchovies", 
+            "mushrooms"]
 
 #Prices for pizza
 prices = [2, 6, 1, 3, 2, 7, 2]
@@ -12,5 +18,17 @@ print(num_two_dollar_slices)
 num_pizzas = len(toppings)
 print(num_pizzas)
 
+#Message for customers
 print("We sell" , num_pizzas ,"different kinds of pizza!")
 
+#2D List of the toppings and prices
+pizza_and_prices = [[2, "pepperoni"], 
+                    [6, "pineapple"], 
+                    [1, "cheese"], 
+                    [3, "sausage"], 
+                    [2, "olives"], 
+                    [7, "anchovies"], 
+                    [2, "mushrooms"]]
+
+#Sorts pizza in ascending order of price
+pizza_and_prices.sort()

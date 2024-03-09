eecd544a9155f3f780e9839d661922fe8ef4b182
Abena Adu <abeanadu@gmail.com>
2024-03-09 15:02:00 -0500

    Used count and length methods

--- pizza.py
+++ pizza.py
@@ -3,3 +3,12 @@ toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies"
 
 #Prices for pizza
 prices = [2, 6, 1, 3, 2, 7, 2]
+
+#Counting how many $2 slices there are 
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+
+#This is the length of the toppings list
+num_pizzas = len(toppings)
+print(num_pizzas)
+
